3fe0151c19bc04ed80321a18578a0ecf687ea87e
SergStra <sergii.gamula@gmail.com>
2020-10-04 13:39:29 +0300

    ready t5

--- test5.js
+++ test5.js
@@ -1,51 +1,55 @@
 // є список продуктів у кошику. Треба вивести на який материках виготовилися продукти у кошикую
 
 const myCart = [
-	{ 
-  	name: 'banana',
-  	quantity: 5,
+  {
+    name: 'banana',
+    quantity: 5,
     company: {
-    	id: 88
-    }
-  },
-  { 
-  	name: 'apple',
-  	quantity: 4,
-    company: {
-    	id: 123
-    }
-  },
-  { 
-  	name: 'milk',
-  	quantity: 15,
-    company: {
-    	id: 999
-    }
-  },
-]
-
-const company = [
-  { 
-  	id: 999,
-  	country: 'Ukraine'
-  },
-  { 
-  	id: 123,
-  	country: 'USA'
+      id: 88,
+    },
   },
   {
-  	id: 88,
-  	country: 'France'
-  }
-]
+    name: 'apple',
+    quantity: 4,
+    company: {
+      id: 123,
+    },
+  },
+  {
+    name: 'milk',
+    quantity: 15,
+    company: {
+      id: 999,
+    },
+  },
+];
 
-const getProductsContinents = (cart) => {
-let cart = new.Map(object.entires(company));
-for (let companies of recipeMap.keys()) {
-    alert(companies);
-alert()
-hgjhgjh
-    // твій код тут
-}
+const company = [
+  {
+    id: 999,
+    country: 'Ukraine',
+  },
+  {
+    id: 123,
+    country: 'USA',
+  },
+  {
+    id: 88,
+    country: 'France',
+  },
+];
 
-const continents = getProductsContinents(myCart)
+const getProductsContinents = (cart, compS) => {
+  cart.forEach((product) => {
+    compS.forEach((Countries) => {
+      if (Object.values(product.company) == Countries.id) {
+        alert(`Продукт ${product.name} виготовляється в країні ${Countries.country}`);
+        alert(Countries.country);
+      }
+    });
+  });
+
+  // твій код тут
+};
+
+const continents = getProductsContinents(myCart, company);
