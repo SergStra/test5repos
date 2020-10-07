3a9b7e7852e80ceeb55db6135b11027661293dbe
oleg-rdk <oleg.buiar.pro@gmail.com>
2020-10-07 10:43:25 +0300

    create diff to comment files

--- test4.js
+++ test4.js
@@ -5,19 +5,19 @@ const myCart = [{ banana: 5 }, { milk: 1 }, { apples: 10 }];
 const myPrices = [{ milk: 6.4 }, { apple: 0.6 }, { banana: 4.6 }];
 debugger;
 const getCartPrice = (cart, prices) => {
-  let sum = 0;
-  cart.forEach((prod) => {
-    sum = 0;
-    prices.forEach((prodpr) => {
-      // alert(Object.keys(prod)[0]);
-      // alert(Object.keys(prodpr)[0]);
-      if (Object.keys(prod)[0] == Object.keys(prodpr)[0]) {
-        sum = sum + Object.values(prod)[0] * Object.values(prodpr)[0];
-      }
+    let sum = 0;
+    cart.forEach((prod) => {
+        sum = 0;
+        prices.forEach((prodpr) => {
+            // alert(Object.keys(prod)[0]);
+            // alert(Object.keys(prodpr)[0]);
+            if (Object.keys(prod)[0] == Object.keys(prodpr)[0]) {
+                sum = sum + Object.values(prod)[0] * Object.values(prodpr)[0];
+            }
+        });
+        alert(`fuuu ${prod} , sum ${sum}`);
     });
-    alert(`fuuu ${prod} , sum ${sum}`);
-  });
-  return sum;
+    return sum;
 };
 // твій код тут
 
